b5eac1643dc7d087b8db771282c53179aa3d138d
Thilo <tfr@e-rat.de>
2020-12-29 16:23:45 +0100

    MainTest

---
+++ Farming/00/main.lua
@@ -0,0 +1,46 @@
+function checkFuel()
+    if turtle.getFuelLevel() < 1 do
+        local currentSlot = turtle.getSelectedSlot();
+        turtle.select(15)
+        if turtle.getItemCount(15) < 1 do
+            term.write("bitte in Slot 15 brennstoff nachlegen\n")
+            while turtle.getItemCount(15) < 1 do
+            end
+        end
+        turtle.refuel(1)
+        turtle.select(currentSlot)
+    end
+end
+
+function moveForward()
+    checkFuel()
+    if turtle.detect() do
+        turtle.dig()
+    end
+    turtle.forward()
+end
+
+function buildGround(xMax,yMax)
+    x = 0
+    y = 0
+    while y < yMax do
+        while x < xMax do
+            x=x-1
+            moveForward()
+        end
+        y=y-1
+        turtle.turnRight()
+        moveForward()
+        turtle.turnRight()
+        x=xMax
+    end
+end
+
+term.write("bitteLängeEingeben\n");
+local xMax = tonumber(io.read());
+term.write("bitteBreiteEingeben\n");
+local yMax = tonumber(io.read());
+buildGround(xMax,yMax)
+
+
+
